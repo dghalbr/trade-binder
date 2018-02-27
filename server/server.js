996fd4b82f8ce6b21cdbbe9927207fdb2e6e7b7d
Dustin Halbrooks <dustin.halbrooks@gmail.com>
2018-02-26 22:04:58 -0600

    Added basic connection to mongodb.

--- server/server.js
+++ server/server.js
@@ -1,14 +1,11 @@
 require('dotenv').config
-
+const MongoClient = require('mongodb').MongoClient;
 const express = require('express');
 const bodyParser = require('body-parser');
-
 const Neo4jApi = require('./neo4j-api');
-
-
 const app = express();
 const db = new Neo4jApi();
-const port = process.env.ENV_PORT;
+const port = process.env.REACT_APP_ENV_PORT;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 /*
@@ -33,5 +30,14 @@ app.post('/clear', (req, res) => {
         .catch(error => res.status(500).send(error));
 });
 */
+
+MongoClient.connect("mongodb://dustin:dustin@ds137110.mlab.com:37110/telligent", function(err, db) {
+  if(!err) {
+    console.log("We are connected");
+  } else {
+    console.log("kent broke everything");
+  }
+});
+
 app.listen(port,
     () => console.log(`Server listening on http://localhost:${port}`));
