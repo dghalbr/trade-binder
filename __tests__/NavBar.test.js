f8762cd48c778fca09fb6bac5345cf7dab9876bb
Dustin Halbrooks <dustin.halbrooks@gmail.com>
2018-02-26 23:31:55 -0600

    Update NavBar.test.js

--- __tests__/NavBar.test.js
+++ __tests__/NavBar.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
-import { NavBar } from '../src/components/NavBar/NavBar';
+import NavBar from '../src/components/NavBar/NavBar';
 import renderer from 'react-test-renderer';
 import toJson from 'enzyme-to-json';
 import { shallow } from 'enzyme';
