4fab3b0d92c910f5190f7261a36f6db8fa044101
neighbor983 <kenneth.ray.baker@gmail.com>
2018-02-28 23:53:29 +0000

    mui/css is good to go

--- src/NavBar/NavBar.js
+++ src/NavBar/NavBar.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import './NavBar.css';
+import Appbar from 'muicss/lib/react/appbar';
 
 export default class NavBar extends Component {
   constructor(props) {
@@ -10,7 +11,40 @@ export default class NavBar extends Component {
   //TODO: May want to make the if logic its own component.
   render() {
     return (
-      <div className="NavBar">
+      <header className="mui-appbar mui--z1">
+        <Appbar className="Appbar">
+          <div className="mui-container">
+            <table>
+              <tr className="mui--appbar-height">
+                <td className="mui--text-title">Trade Binder</td>
+                <td className="mui--text-right">
+                {!this.props.isLoggedIn ? (
+                  <ul className="mui-list--inline mui--text-body2">
+                    <li><Link to="/">Home</Link></li>
+                    <li><Link to="/login">Login</Link></li>
+                    <li><Link to="/register">Register</Link></li>
+                  </ul>
+                ) : (
+                  <ul className="mui-list--inline mui--text-body2">
+                    <li><Link to="/">Home</Link></li>
+                    <li>
+                      <Link to="/login" onClick={this.props.logout}>
+                        Logout
+                        </Link>
+                    </li>
+                  </ul>
+                )}
+                </td>
+              </tr>
+            </table>
+          </div>
+        </Appbar>
+      </header>
+    );
+  }
+}
+/*
+<div className="NavBar">
         <div className="NavBar-header">
           <h2>Trade Binder</h2>
           <span className="NavBar-nav">
@@ -35,6 +69,4 @@ export default class NavBar extends Component {
         </div>
         <br />
       </div>
-    );
-  }
-}
+      */
