a62e1c45a62e784b3e0aa91e2b9d02e79ac4e72d
Dustin Halbrooks <dustin.halbrooks@gmail.com>
2018-02-28 18:24:45 -0600

    Pulled out api key stuff to .env.

--- src/Auth/Auth.js
+++ src/Auth/Auth.js
@@ -1,16 +1,18 @@
 import * as firebase from 'firebase';
+import dotenv from 'dotenv';
+dotenv.config({ silent: true });
 
 let defaultApp = null;
 let loggedIn = false;
 
 //Not sure this should be on the client as plain text?
 defaultApp = firebase.initializeApp({
-  apiKey: 'AIzaSyCJ08xZ2o-yfj1h9SF-DdSJMYWipYGqjak',
-  authDomain: 'trade-binder.firebaseapp.com',
-  databaseURL: 'https://trade-binder.firebaseio.com',
-  projectId: 'trade-binder',
-  storageBucket: 'trade-binder.appspot.com',
-  messagingSenderId: '452350586373'
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID
 });
 
 export default class Auth {
