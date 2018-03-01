f5330ed825ecc20d8dcef4da20a18de8439ea628
neighbor983 <kenneth.ray.baker@gmail.com>
2018-03-01 05:36:21 +0000

    changed the navbar up will more then likely use the hamburger bar for a drawer but that is a later problem

--- src/NavBar/NavBar.js
+++ src/NavBar/NavBar.js
@@ -2,6 +2,10 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import './NavBar.css';
 import AppBar from 'material-ui/AppBar';
+import MenuItem from 'material-ui/MenuItem';
+import IconMenu from 'material-ui/IconMenu';
+import IconButton from 'material-ui/IconButton';
+import MoreVertIcon from 'material-ui/svg-icons/navigation/more-vert';
 
 export default class NavBar extends Component {
   constructor(props) {
@@ -11,36 +15,32 @@ export default class NavBar extends Component {
   //TODO: May want to make the if logic its own component.
   render() {
     return (
-      <AppBar className="Appbar">
-        <nav>
-          {!this.props.isLoggedIn ? (
-            <ul>
-              <li>Trade Binder</li>
-              <li>
-                <Link to="/">Home</Link>
-              </li>
-              <li>
-                <Link to="/login">Login</Link>
-              </li>
-              <li>
-                <Link to="/register">Register</Link>
-              </li>
-            </ul>
-          ) : (
-            <ul>
-              <li>Trade Binder</li>
-              <li>
-                <Link to="/">Home</Link>
-              </li>
-              <li>
-                <Link to="/login" onClick={this.props.logout}>
-                  Logout
-                </Link>
-              </li>
-            </ul>
+      <AppBar className="Appbar"
+        title='TradeBinder'
+        iconElementRight={!this.props.isLoggedIn ? (
+          <IconMenu iconButtonElement={<IconButton><MoreVertIcon /></IconButton>}
+                    targetOrigin={{horizontal: 'right', vertical: 'top'}}
+                    anchorOrigin={{horizontal: 'right', vertical: 'top'}}>
+                    <MenuItem primaryText="Home" 
+                              containerElement={<Link to="/" />}/>
+                    <MenuItem primaryText="Login" 
+                              containerElement={<Link to="/login" />}/>
+                    <MenuItem primaryText="Register" 
+                              containerElement={<Link to="/register" />}/>
+          </IconMenu>
+        ) : (
+          <IconMenu iconButtonElement={<IconButton><MoreVertIcon /></IconButton>}
+                    targetOrigin={{horizontal: 'right', vertical: 'top'}}
+                    anchorOrigin={{horizontal: 'right', vertical: 'top'}}>
+                    <MenuItem primaryText="Home" 
+                              containerElement={<Link to="/" />}/>
+                    <MenuItem primaryText="Logout" 
+                              containerElement={<Link to="/login" />}
+                              onClick={this.props.logout}/>
+          </IconMenu>
           )}
-        </nav>
-      </AppBar>
+       />
+        
     );
   }
 }
