61f516a7d3a06d4c07e179bf39d4f3454cc16656
Dustin Halbrooks <dustin.halbrooks@protonmail.com>
2018-03-01 15:40:14 +0000

    Fix tests

--- src/NavBar/NavBar.js
+++ src/NavBar/NavBar.js
@@ -15,32 +15,40 @@ export default class NavBar extends Component {
   //TODO: May want to make the if logic its own component.
   render() {
     return (
-      <AppBar className="Appbar"
-        title='TradeBinder'
-        iconElementRight={!this.props.isLoggedIn ? (
-          <IconMenu iconButtonElement={<IconButton><MoreVertIcon /></IconButton>}
-                    targetOrigin={{horizontal: 'right', vertical: 'top'}}
-                    anchorOrigin={{horizontal: 'right', vertical: 'top'}}>
-                    <MenuItem primaryText="Home" 
-                              containerElement={<Link to="/" />}/>
-                    <MenuItem primaryText="Login" 
-                              containerElement={<Link to="/login" />}/>
-                    <MenuItem primaryText="Register" 
-                              containerElement={<Link to="/register" />}/>
-          </IconMenu>
-        ) : (
-          <IconMenu iconButtonElement={<IconButton><MoreVertIcon /></IconButton>}
-                    targetOrigin={{horizontal: 'right', vertical: 'top'}}
-                    anchorOrigin={{horizontal: 'right', vertical: 'top'}}>
-                    <MenuItem primaryText="Home" 
-                              containerElement={<Link to="/" />}/>
-                    <MenuItem primaryText="Logout" 
-                              containerElement={<Link to="/login" />}
-                              onClick={this.props.logout}/>
-          </IconMenu>
-          )}
-       />
-        
+      <AppBar
+        className="Appbar"
+        title="TradeBinder"
+        iconElementRight={
+          !this.props.isLoggedIn ? (
+            <IconMenu
+              iconButtonElement={
+                <IconButton>
+                  <MoreVertIcon />
+                </IconButton>
+              }
+              targetOrigin={{ horizontal: 'right', vertical: 'top' }}
+              anchorOrigin={{ horizontal: 'right', vertical: 'top' }}
+            >
+              <MenuItem primaryText="Home" containerElement={<Link to="/" />} />
+              <MenuItem primaryText="Login" containerElement={<Link to="/login" />} />
+              <MenuItem primaryText="Register" containerElement={<Link to="/register" />} />
+            </IconMenu>
+          ) : (
+            <IconMenu
+              iconButtonElement={
+                <IconButton>
+                  <MoreVertIcon />
+                </IconButton>
+              }
+              targetOrigin={{ horizontal: 'right', vertical: 'top' }}
+              anchorOrigin={{ horizontal: 'right', vertical: 'top' }}
+            >
+              <MenuItem primaryText="Home" containerElement={<Link to="/" />} />
+              <MenuItem primaryText="Logout" containerElement={<Link to="/login" />} onClick={this.props.logout} />
+            </IconMenu>
+          )
+        }
+      />
     );
   }
 }
