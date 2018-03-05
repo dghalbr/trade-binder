fc7983f60b3967f83996abe435644065efcdc34b
neighbor983 <kenneth.ray.baker@gmail.com>
2018-03-05 06:11:15 +0000

    card Collection table framework

--- src/Account/Account.js
+++ src/Account/Account.js
@@ -2,6 +2,10 @@ import React, { Component } from 'react';
 import './Account.css';
 import RaisedButton from 'material-ui/RaisedButton';
 import TextField from 'material-ui/TextField';
+import { Table, TableBody, TableHeader, TableHeaderColumn, TableRow, TableRowColumn } from 'material-ui/Table';
+
+import Minus from 'material-ui/svg-icons/navigation/arrow-back';
+import Plus from 'material-ui/svg-icons/navigation/arrow-forward';
 
 export default class Account extends Component {
   constructor(props) {
@@ -38,6 +42,35 @@ export default class Account extends Component {
           id="updatePasswordButton"
           onClick={this.passwordUpdate}
         />
+
+        <Table>
+          <TableHeader displaySelectAll={false} adjustForCheckbox={false}>
+            <TableRow>
+              <TableHeaderColumn className="firstColumn">ID</TableHeaderColumn>
+              <TableHeaderColumn>Name</TableHeaderColumn>
+              <TableHeaderColumn>Set</TableHeaderColumn>
+              <TableHeaderColumn className="wantColumn">Want</TableHeaderColumn>
+              <TableHeaderColumn className="tradeColumn">Trade</TableHeaderColumn>
+            </TableRow>
+          </TableHeader>
+          <TableBody displayRowCheckbox={false}>
+            <TableRow selectable={false}>
+              <TableRowColumn className="firstColumn">1</TableRowColumn>
+              <TableRowColumn>Jace the Mind Sculptor</TableRowColumn>
+              <TableRowColumn>Worldwake</TableRowColumn>
+              <TableRowColumn className="valign-wrapper wantColumn">
+                <Minus className="iconStyles" />
+                <span className="iconStyles">4</span>
+                <Plus className="iconStyles" />
+              </TableRowColumn>
+              <TableRowColumn className="valign-wrapper tradeColumn">
+                <Minus className="iconStyles" />
+                <span className="iconStyles">0</span>
+                <Plus className="iconStyles" />
+              </TableRowColumn>
+            </TableRow>
+          </TableBody>
+        </Table>
       </div>
     );
   }
