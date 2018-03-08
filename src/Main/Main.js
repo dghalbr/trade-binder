4514bd0c45ed271e54e280e7ae7d331ab3b44902
Dustin Halbrooks <dustin.halbrooks@gmail.com>
2018-03-08 10:30:03 -0600

    (backup)

--- src/Main/Main.js
+++ src/Main/Main.js
@@ -3,21 +3,51 @@ import { Router, Route, Switch, Redirect } from 'react-router-dom';
 import Home from '../Home/Home';
 import Login from '../Login/Login';
 import Register from '../Register/Register';
-import Auth from '../Auth/Auth';
+// import Auth from '../Auth/Auth';
 import Account from '../Account/Account';
 import NavBar from '../NavBar/NavBar';
 import createBrowserHistory from 'history/createBrowserHistory';
+import { firebaseAuth } from '../config/constants';
 
 const history = createBrowserHistory();
-const auth = new Auth();
+// const auth = new Auth();
+
+//TODO: CLEAN UP! get rid of isloggedin if we use authed or vice versa
+
+function PrivateRoute({ component: Component, exact, strict, path, authed, ...rest }) {
+  console.log('authed: ', authed);
+  console.log('rest: ', rest);
+  return (
+    <Route
+      exact={exact}
+      strict={strict}
+      path={path}
+      {...rest}
+      render={props =>
+        authed === true ? (
+          <Component {...props} {...rest} />
+        ) : (
+          <Redirect to={{ pathname: '/login', state: { from: props.location } }} />
+        )
+      }
+    />
+  );
+}
+
+function PublicRoute({ component: Component, ...rest }) {
+  console.log('rest: ', rest);
+  return <Route {...rest} render={props => <Component {...props} />} />;
+}
 
 export default class Main extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      isLoggedIn: auth.isLoggedIn(),
+      isLoggedIn: firebaseAuth().currentUser ? true : false,
       appDrawerOpen: false,
-      user: auth.getCurrentUser()
+      user: firebaseAuth().currentUser,
+      authed: firebaseAuth().currentUser ? true : false,
+      loading: true
     };
     this.login = this.login.bind(this);
     this.logout = this.logout.bind(this);
@@ -27,14 +57,33 @@ export default class Main extends Component {
     this.passwordReset = this.passwordReset.bind(this);
   }
 
+  componentDidMount() {
+    this.removeListener = firebaseAuth().onAuthStateChanged(user => {
+      if (user) {
+        this.setState({
+          authed: true,
+          loading: false
+        });
+      } else {
+        this.setState({
+          authed: false,
+          loading: false
+        });
+      }
+    });
+  }
+  componentWillUnmount() {
+    this.removeListener();
+  }
+
   componentDidUpdate() {
     /*
-     * DGH - Auth.js isn't a React Component so if a user
+     * DGH - firebaseAuth.js isn't a React Component so if a user
      * becomes logged in or out we need to re-render those 
      * buttons. 
      */
-    if (this.state.isLoggedIn !== auth.isLoggedIn()) {
-      this.setState({ ...this.state, isLoggedIn: !this.state.isLoggedIn });
+    if (this.state.authed !== this.state.isLoggedIn) {
+      this.setState({ ...this.state, isLoggedIn: this.state.authed });
     }
   }
 
@@ -55,20 +104,15 @@ export default class Main extends Component {
               <Route exact path="/" render={state => <Home />} />
               <Route
                 path="/login"
-                render={state => (
-                  <Login user={this.state.user} handleLogin={this.login} passwordReset={this.passwordReset} />
-                )}
+                render={state => <Login handleLogin={this.login} passwordReset={this.passwordReset} />}
               />
               <Route path="/register" render={state => <Register handleRegister={this.register} />} />
-              <Route
+              <PrivateRoute
+                authed={this.state.authed}
                 path="/account"
-                render={state =>
-                  this.state.user ? (
-                    <Account user={this.state.user} passwordUpdate={this.passwordUpdate} />
-                  ) : (
-                    <Redirect to="/login" />
-                  )
-                }
+                user={firebaseAuth().currentUser}
+                passwordUpdate={this.passwordUpdate}
+                component={Account}
               />
             </Switch>
           </div>
@@ -78,14 +122,22 @@ export default class Main extends Component {
   }
 
   /**
+   * 
+              <Route path="/login" handleLogin={this.login}
+                passwordReset={this.passwordReset} component={Login} />
+   * 
+   * 
+   * 
+   * 
+   * 
    * Login to the application via Firebase
    * @param  {string} username
    * @param  {string} password
    */
   login(username, password) {
-    if (!auth.isLoggedIn()) {
-      auth
-        .doSignInAndRetrieveDataWithEmailAndPassword(username, password)
+    if (!firebaseAuth().currentUser) {
+      firebaseAuth()
+        .signInAndRetrieveDataWithEmailAndPassword(username, password)
         .then(user => {
           this.setState({ ...this.state, user: user.user, isLoggedIn: true, appDrawerOpen: false });
           history.push({ pathname: '/account' });
@@ -108,10 +160,12 @@ export default class Main extends Component {
    * Logout of the application via Firebase
    */
   logout() {
-    auth.doSignOut().then(() => {
-      this.setState({ ...this.state, user: null, isLoggedIn: false, appDrawerOpen: false });
-      history.push({ pathname: '/login' });
-    });
+    firebaseAuth()
+      .signOut()
+      .then(() => {
+        this.setState({ ...this.state, user: null, isLoggedIn: false, appDrawerOpen: false });
+        history.push({ pathname: '/login' });
+      });
   }
 
   /**
@@ -120,8 +174,8 @@ export default class Main extends Component {
    * @param  {string} password
    */
   register(username, password) {
-    auth
-      .doCreateUserWithEmailAndPassword(username, password)
+    firebaseAuth
+      .signInWithEmailAndPassword(username, password)
       .then(user => {
         this.setState({ ...this.state, user: user, isLoggedIn: true, appDrawerOpen: false });
         history.push({ pathname: '/account' });
@@ -142,14 +196,14 @@ export default class Main extends Component {
    * @param  {string} password
    */
   passwordUpdate(password) {
-    return this.state.user.updatePassword(password);
+    return firebaseAuth().currentUser.updatePassword(password);
   }
   /**
    * Reset a User's password via Firebase
    * @param  {string} email
    */
   passwordReset(email) {
-    return auth.doPasswordReset(email);
+    return firebaseAuth().sendPasswordResetEmail(email);
   }
 
   /**
